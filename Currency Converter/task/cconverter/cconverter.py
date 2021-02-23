04baab4c5b9c06f65cc19f5f13d65ce8ee0b90f2
IgnatIvanov <ignativanov1996@mail.ru>
2021-02-23 23:25:45 +0300

    2021.02.23.2325
    Stage 4 complete.

--- Currency Converter/task/cconverter/cconverter.py
+++ Currency Converter/task/cconverter/cconverter.py
@@ -1,4 +1,16 @@
 # write your code here!
 
-conicoins = float(input('Please, enter the number of conicoins you have: '))
-exchange_rate = float(input('Please, enter the exchange rate: '))
+# conicoins = float(input('Please, enter the number of conicoins you have: '))
+# exchange_rate = float(input('Please, enter the exchange rate: '))
+
+exchange_rate = {}
+exchange_rate['RUB'] = 2.98
+exchange_rate['ARS'] = 0.82
+exchange_rate['HNL'] = 0.17
+exchange_rate['AUD'] = 1.9622
+exchange_rate['MAD'] = 0.208
+
+conicoins = float(input())
+
+for (key,value) in exchange_rate.items():
+    print('I will get {} {} from the sale of {} conicoins.'.format(round(value * conicoins, 2), key, conicoins))
