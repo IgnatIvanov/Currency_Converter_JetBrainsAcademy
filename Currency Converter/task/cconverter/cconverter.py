ff63641e683ddda09f7f0d16bafff06833fa7bd5
IgnatIvanov <ignativanov1996@mail.ru>
2021-02-23 20:28:05 +0300

    2021.02.23.2027
    Stages 1 and 2 comlete.

--- Currency Converter/task/cconverter/cconverter.py
+++ Currency Converter/task/cconverter/cconverter.py
@@ -1,2 +1,7 @@
 # write your code here!
-print('Meet a conicoin!')
+# print('Meet a conicoin!')
+conicoins = int(input())
+
+print('I have', conicoins, 'conicoins.')
+print('{} conicoins cost {} dollars.'.format(conicoins, conicoins * 100))
+print('I am rich! Yippee!')
